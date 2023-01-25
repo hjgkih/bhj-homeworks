a2a9eafcd7357cf362c59de207d141aa87f2abe5
hjgkih <melyanovleg@gmail.com>
2023-01-25 15:30:41 +0300

    first task of first homework

---
+++ js-features/countdown/task.js
@@ -0,0 +1,49 @@
+const timerValueHour = document.getElementById("timer__hour");   // 0
+const timerValueMinute = document.getElementById("timer__minute"); // 0
+const timerValueSecond = document.getElementById("timer__second"); // 5
+const timerValueHourStart = (Number(timerValueHour.textContent));
+const timerValueMinuteStart = (Number(timerValueMinute.textContent));
+const timerValueSecondStart = (Number(timerValueSecond.textContent));
+
+setInterval(() => {
+    if (timerValueHour.textContent == 0 && timerValueMinute.textContent == 0 && timerValueSecond.textContent == 0) {
+        alert("Вы победили в конкурсе!");
+        timerValueHour.textContent = timerValueHourStart;
+        timerValueMinute.textContent = timerValueMinuteStart;
+        timerValueSecond.textContent = timerValueSecondStart;
+        window.location.href = 'https://netology.ru'
+    } else if (timerValueSecond.textContent <= 0) {
+        timerValueMinute.textContent--;
+        timerValueSecond.textContent = 59;
+    } else if (timerValueMinute.textContent < 0) {
+        timerValueHour.textContent--;
+        timerValueMinute.textContent = 59;
+    } else {
+        timerValueSecond.textContent--;
+    }
+}, 100)
+
+
+
+
+
+
+
+
+
+
+
+
+// // ---------------Первое задание 
+
+// const timerValue = document.getElementById("timer");
+// const timerValueStart = (Number(timerValue.textContent));
+
+// setInterval(() => {
+//     if (timerValue.textContent <= 0) {
+//         alert("Вы победили в конкурсе!");
+//         timerValue.textContent = timerValueStart;
+//     } else {
+//     timerValue.textContent--;
+//     }
+// }, 100)
