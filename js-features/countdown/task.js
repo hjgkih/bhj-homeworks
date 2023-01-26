4cc3c55ec3e101bb3a2b6204a87e3f9cfd63f529
hjgkih <melyanovleg@gmail.com>
2023-01-26 11:18:11 +0300

    first task is done in 3 variants

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -29,7 +29,36 @@ setInterval(() => {
     } else if (timerValueSecond.textContent.length < 2) {
         timerValueSecond.textContent = ('0' +  timerValueSecond.textContent).slice(-2);
       }   
-}, 1000)
+}, 0)
+
+// // --------------------Реализация с помощью Date
+// let timerValueHour = document.getElementById("timer__hour");    
+// let timerValueMinute = document.getElementById("timer__minute"); 
+// let timerValueSecond = document.getElementById("timer__second"); 
+// const startTime = (new Date()).getTime() + 6000;
+
+// function getTime(endTime) {
+//   let t = endTime - new Date(); 
+//   let seconds = Math.floor( (t/1000) % 60 );  
+//   let minutes = Math.floor( (t/1000/60) % 60 );  
+//   let hours = Math.floor( (t/(1000*60*60)) % 24 );  
+//   return {
+//      'hours': hours,  
+//      'minutes': minutes,  
+//      'seconds': seconds  
+//   }
+// } 
+
+// setInterval (() => {
+//   if ( (timerValueSecond.textContent == '00' && timerValueMinute.textContent == '00' && timerValueHour.textContent == '00')) {
+//     alert("Вы победили в конкурсе!");
+//     window.location.href = 'https://netology.ru'
+//     clearInterval(intervalId);
+//   }
+//   timerValueSecond.textContent = ('0' + getTime(startTime).seconds).slice(-2);
+//   timerValueMinute.textContent = ('0' + getTime(startTime).minutes).slice(-2);
+//   timerValueHour.textContent = ('0' + getTime(startTime).hours).slice(-2);
+// }, 1000) 
 
 
 // // ---------------Первое задание 
