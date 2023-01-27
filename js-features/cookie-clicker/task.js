23720aaaa91dae0590871c79284ea4f529b08897
hjgkih <melyanovleg@gmail.com>
2023-01-27 11:56:25 +0300

    optimized with ternary construstion

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,7 +1,7 @@
 const element = document.getElementById("cookie");
 const counter = document.getElementById("clicker__counter");
 const speed = document.getElementById("clicker__speed")
-const elementWidthStart = Number(element.width);
+// const elementWidthStart = Number(element.width);
 const timeActual = Date.now();
 
 element.onclick = function () {
@@ -9,11 +9,8 @@ element.onclick = function () {
   const timeNow = Date.now();
   speed.textContent = (counter.textContent / (timeNow - timeActual) * 1000).toFixed(2);
   
-  if (counter.textContent % 2 !== 0) {
-    element.width = 300;
-  } else {
-      element.width = elementWidthStart;
-    }
+
+  element.width = counter.textContent % 2 ? 300 : 200;
+
+
 }
-
-
