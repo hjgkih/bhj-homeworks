e99c806fa49145e4fa235d9556d5f6f6c5d7df5e
hjgkih <melyanovleg@gmail.com>
2023-04-26 15:39:34 +0300

    Second task is done

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -17,14 +17,14 @@ class Game {
   }
 
   registerEvents() {
-    /*
-      TODO:
-      Написать обработчик события, который откликается
-      на каждый введённый символ.
-      В случае правильного ввода слова вызываем this.success()
-      При неправильном вводе символа - this.fail();
-      DOM-элемент текущего символа находится в свойстве this.currentSymbol.
-     */
+    const onKey = (i) => {
+     if (i.key.toLowerCase() === this.currentSymbol.textContent.toLowerCase()){
+      this.success();
+     } else {
+      this.fail();
+     }
+    }
+    document.addEventListener('keyup', onKey); 
   }
 
   success() {
