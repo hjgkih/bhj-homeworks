3d5db76681846ba6d2434741baa7a819c9fc9c8c
hjgkih <melyanovleg@gmail.com>
2023-05-03 22:55:27 +0300

    code refactored

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -2,18 +2,31 @@ const reveal = Array.from(document.querySelectorAll('.reveal'));
 
 reveal.forEach((i) => {
   window.addEventListener('scroll', () => {
-    const { top, bottom } = i.getBoundingClientRect();
-    
-    if (i.className == 'reveal reveal_active') {
-        i.className = 'reveal';
+  const { innerHeight } = window;
+  const { top } = i.getBoundingClientRect();
+  if (top < innerHeight && top > 0) {
+    i.classList.add("reveal_active");
+  } else {
+      i.classList.remove("reveal_active");
     }
-    if (bottom < 0) {
-      return false;
-    }
-
-    if (top > window.innerHeight) {
-      return false;
-    }
-    i.className += ' reveal_active';        
   })
 })
+
+// reveal.forEach((i) => {
+//   window.addEventListener('scroll', () => {
+//     const { top, bottom } = i.getBoundingClientRect();
+    
+//     if (i.className == 'reveal reveal_active') {
+//         i.className = 'reveal';
+//     }
+//     if (bottom < 0) {
+//       return false;
+//     }
+
+//     if (top > window.innerHeight) {
+//       return false;
+//     }
+//     i.className += ' reveal_active';        
+//   })
+// })
+
